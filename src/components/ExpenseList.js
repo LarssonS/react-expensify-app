b9f84772f4ec27beed145d173d7be2790e0d74cf
LarssonS <Stefanlarsson95@hotmail.com>
2018-09-12 11:45:10 +0200

    Added no expenses feature

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,6 +6,7 @@ import selectExpenses from '../selectors/expenses';
 const ExpenseList = (props) => (
     <div>
         <h1>ExpenseList</h1>
+        {props.expenses.length === 0 && <p>No expenses</p>}
         {props.expenses.map((expense) => {
                 return <ExpenseListItem key={expense.id} {...expense}/>
             })}
