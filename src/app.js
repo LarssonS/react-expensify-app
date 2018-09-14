ec146966e3dc06d372c6a86402679b828b19133c
LarssonS <Stefanlarsson95@hotmail.com>
2018-09-14 16:05:33 +0200

    Add private routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 );
+let hasRenderd = false;
+const renderApp = () => {
+    if (!hasRenderd) {
+        ReactDOM.render(jsx, document.querySelector('#app'));
+        hasRenderd = true;
+    }
+}
 
 ReactDOM.render(<p>Loading...</p>, document.querySelector('#app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.querySelector('#app'));
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
 });
